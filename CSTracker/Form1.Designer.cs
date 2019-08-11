52316937776ee5d3a1d2e14baba2f0a52986233c
Rich Aisabor <raisabor@smu.edu>
2019-08-11 16:54:05 -0500

    Add project files.

---
+++ CSTracker/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace CSTracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imgCamUser = new Emgu.CV.UI.ImageBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtXYRadius = new System.Windows.Forms.RichTextBox();
+            this.ButtonPauseOrResume = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imgCamUser)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgCamUser
+            // 
+            this.imgCamUser.Location = new System.Drawing.Point(106, 36);
+            this.imgCamUser.Name = "imgCamUser";
+            this.imgCamUser.Size = new System.Drawing.Size(712, 395);
+            this.imgCamUser.TabIndex = 2;
+            this.imgCamUser.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(225, 514);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 36);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // txtXYRadius
+            // 
+            this.txtXYRadius.Location = new System.Drawing.Point(570, 469);
+            this.txtXYRadius.Name = "txtXYRadius";
+            this.txtXYRadius.Size = new System.Drawing.Size(248, 96);
+            this.txtXYRadius.TabIndex = 5;
+            this.txtXYRadius.Text = "";
+            // 
+            // ButtonPauseOrResume
+            // 
+            this.ButtonPauseOrResume.Location = new System.Drawing.Point(106, 514);
+            this.ButtonPauseOrResume.Name = "ButtonPauseOrResume";
+            this.ButtonPauseOrResume.Size = new System.Drawing.Size(92, 36);
+            this.ButtonPauseOrResume.TabIndex = 6;
+            this.ButtonPauseOrResume.Text = "pause";
+            this.ButtonPauseOrResume.UseVisualStyleBackColor = true;
+            this.ButtonPauseOrResume.Click += new System.EventHandler(this.ButtonPauseOrResume_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1103, 621);
+            this.Controls.Add(this.ButtonPauseOrResume);
+            this.Controls.Add(this.txtXYRadius);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.imgCamUser);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgCamUser)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+       
+        private Emgu.CV.UI.ImageBox imgCamUser;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox txtXYRadius;
+        private System.Windows.Forms.Button ButtonPauseOrResume;
+    }
+}
+
---
+++ CSTracker/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+using System.Windows.Threading;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+
+namespace CSTracker
+{
+    public partial class Form1 : Form
+    {
+        //member variables
+
+        private VideoCapture capture;
+
+        private CascadeClassifier cascadeClassifier;
+
+        private bool DetectionInProgress;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            try
+            {
+                capture = new VideoCapture();
+                imgCamUser.Image = capture.QueryFrame();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("unable to read from webcam, error: " + Environment.NewLine + Environment.NewLine +
+                                ex.Message + Environment.NewLine + Environment.NewLine +
+                                "exiting program");
+                Environment.Exit(0);
+                return;
+
+
+            }
+
+            Application.Idle += processFrameUpdateGUI;
+            DetectionInProgress = true;
+
+        }
+
+       
+
+        void processFrameUpdateGUI(object sender, EventArgs arg)
+        {
+
+            cascadeClassifier = new CascadeClassifier(Application.StartupPath + "/opencv/data/haarcascades/haarcascade_frontalface_default.xml");
+
+            using (var imageFrame = capture.QueryFrame().ToImage<Bgr, Byte>())
+            {
+                if (imageFrame != null)
+                {
+                    var grayframe = imageFrame.Convert<Gray, Byte>();
+                    var faces = cascadeClassifier.DetectMultiScale(grayframe, 1.1, 10, Size.Empty); //the actual face detection happens here
+                    foreach (var face in faces)
+                    {
+
+                        if (txtXYRadius.Text != "")
+                        {
+                            txtXYRadius.AppendText(Environment.NewLine);        
+                        }
+
+                        txtXYRadius.AppendText("face position = x " + face.X.ToString().PadLeft(4) + ", y = " + face.Y.ToString().PadLeft(4));          // Prints face position to text box                  
+
+                        txtXYRadius.ScrollToCaret();                
+
+                        imageFrame.Draw(face, new Bgr(Color.BurlyWood), 3);
+
+                    }
+                }
+                imgCamUser.Image = imageFrame;
+            }
+
+        }
+
+        private void ButtonPauseOrResume_Click(object sender, EventArgs e)
+        {
+            if (DetectionInProgress == true)                      
+            {
+                Application.Idle -= processFrameUpdateGUI;       
+                DetectionInProgress = false;
+                ButtonPauseOrResume.Text = " resume ";
+            }
+            else
+            { 
+                Application.Idle += processFrameUpdateGUI;       
+                DetectionInProgress = true;                       
+                ButtonPauseOrResume.Text = " pause ";                 
+            }
+        }
+
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+
+            SaveFileDialog dialog = new SaveFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (Bitmap image = capture.QueryFrame().Bitmap)
+                    {
+                        image.Save(dialog.FileName + ".jpeg", ImageFormat.Jpeg);
+                    }
+                    MessageBox.Show("Picture Saved Successfully.");
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An Error has occured: \n" + ex.Message);
+                }
+            }
+
+        }
+
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
+
